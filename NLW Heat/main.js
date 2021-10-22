716872cdd79f0d0517b525677f88064a1d9984bd
thiagopolettz <thiagojmpoletto@gmail.com>
2021-10-22 19:43:19 -0300

    Version 1 - Versão 1
    Html/CSS/Js code

---
+++ NLW Heat/main.js
@@ -0,0 +1,33 @@
+const socialMidias = {
+  github: 'thiagopolettz',
+  facebook: 'thiago.josemoserpoletto',
+  instagram: 'thiagopoletto',
+  twitter: 'redlikke'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    // console.log(social)
+    li.children[0].href = `https://${social}.com/${socialMidias[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubAPI() {
+  const url = `https://api.github.com/users/${socialMidias.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userUrl.href = data.html_url
+      userPic.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+  console.log(url)
+}
+
+getGithubAPI()
